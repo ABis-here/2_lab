1dc0a428579cb7c15f0d99a709a9fe67b75059ea
ABis-here <augustasbaublys14@gmail.com>
2025-10-05 23:07:07 +0300

    Fix: include spaces in punctuation count

--- app/src/main/java/com/example/a2lab/utils/TextCounter.java
+++ app/src/main/java/com/example/a2lab/utils/TextCounter.java
@@ -41,7 +41,7 @@ public class TextCounter {
         int c = 0;
         for (int i = 0; i < input.length(); i++) {
             char ch = input.charAt(i);
-            if (ch == ',' || ch == '.') c++;
+            if (ch == ' ' || ch == ',' || ch == '.') c++;
         }
         return c;
     }
